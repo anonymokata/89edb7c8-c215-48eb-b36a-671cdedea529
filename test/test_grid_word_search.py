f90fd59d9464865cbfb4947368ecdddc9b17fd6f
someone <someone>
2019-10-21 23:04:54 -0400

    CHANGE Refactor TestGridWordSearch
    * Move repeating code to private method
    * Add full width/height words to abc_grid_*_words.csv files

--- test/test_grid_word_search.py
+++ test/test_grid_word_search.py
@@ -44,65 +44,7 @@ class TestGridWordSearch(unittest.TestCase):
             logger_helper.format_log(classname=self.classname, method=method, msg="Started ------------------")
         )
 
-        try:
-            self.log.info(
-                logger_helper.format_log(classname=self.classname, method=method, msg=f"Using {csv_file}")
-            )
-            repo = WordSearchRepository(csv_file=csv_file)
-            controller = WordSearchController(grid=repo.get_word_search().get_grid())
-
-            not_found_count = len(repo.get_word_search().get_word_list())
-            for w in repo.get_word_search().get_word_list():
-                self.log.info(
-                    logger_helper.format_log(classname=self.classname, method=method, msg=f"Searching for {w}")
-                )
-                coord_list = controller.find_word(word_as_chars=w)
-                if len(coord_list) > 0:
-                    not_found_count -= 1
-                    self.log.info(
-                        logger_helper.format_log(classname=self.classname, method=method, msg=f"{w} FOUND")
-                    )
-                    c_string = ""
-                    for c in coord_list:
-                        c_string += f"{str(c)} "
-                    self.log.info(
-                        logger_helper.format_log(classname=self.classname, method=method, msg=f"{c_string} FOUND")
-                    )
-                else:
-                    self.log.warning(
-                        logger_helper.format_log(classname=self.classname, method=method, msg=f"{w} NOT FOUND")
-                    )
-
-            if not_found_count > 0:
-                msg = f"{not_found_count} of {len(repo.get_word_search().get_word_list())} words not found in {csv_file}."
-                self.log.error(
-                    logger_helper.format_log(classname=self.classname, method=method, msg=msg)
-                )
-                self.fail(msg)
-            else:
-                msg = f"{len(repo.get_word_search().get_word_list())} were found in {csv_file}."
-                self.log.info(
-                    logger_helper.format_log(classname=self.classname, method=method, msg=msg)
-                )
-
-        except FileFormatException as ffe:
-            msg = f"Received Exception of [{ffe}] for {csv_file}"
-            self.log.error(
-                logger_helper.format_log(classname=self.classname, method=method, msg=msg)
-            )
-            self.fail(msg)
-        except WordLengthException as wle:
-            msg = f"Received Exception of [{wle}] for {csv_file}"
-            self.log.error(
-                logger_helper.format_log(classname=self.classname, method=method, msg=msg)
-            )
-            self.fail(msg)
-        except GridDimensionException as gde:
-            msg = f"Received Exception of [{gde}] for {csv_file}"
-            self.log.error(
-                logger_helper.format_log(classname=self.classname, method=method, msg=msg)
-            )
-            self.fail(msg)
+        self.__main_test_executor(csv_file=csv_file)
 
         self.log.debug(
             logger_helper.format_log(classname=self.classname, method=method, msg="Completed ----------------")
@@ -116,6 +58,16 @@ class TestGridWordSearch(unittest.TestCase):
             logger_helper.format_log(classname=self.classname, method=method, msg="Started ------------------")
         )
 
+        self.__main_test_executor(csv_file=csv_file)
+
+        self.log.debug(
+            logger_helper.format_log(classname=self.classname, method=method, msg="Completed ----------------")
+        )
+        return
+
+    def __main_test_executor(self, csv_file=None):
+        method = '__main_test_executor'
+
         try:
             self.log.info(
                 logger_helper.format_log(classname=self.classname, method=method, msg=f"Using {csv_file}")
@@ -152,7 +104,7 @@ class TestGridWordSearch(unittest.TestCase):
                 )
                 self.fail(msg)
             else:
-                msg = f"{len(repo.get_word_search().get_word_list())} were found in {csv_file}."
+                msg = f"All {len(repo.get_word_search().get_word_list())} words were found in {csv_file}."
                 self.log.info(
                     logger_helper.format_log(classname=self.classname, method=method, msg=msg)
                 )
@@ -175,10 +127,6 @@ class TestGridWordSearch(unittest.TestCase):
                 logger_helper.format_log(classname=self.classname, method=method, msg=msg)
             )
             self.fail(msg)
-
-        self.log.debug(
-            logger_helper.format_log(classname=self.classname, method=method, msg="Completed ----------------")
-        )
         return
 
 
